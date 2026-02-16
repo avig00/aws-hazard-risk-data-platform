29cf4a40a798cfec8af1df7e0e1cde72a5d6545c
avig00 <vigamogh@gmail.com>
2026-02-15 23:33:45 -0600

    code clean up

--- etl_legacy/gold/build_gold_marts.py
+++
@@ -1,8 +0,0 @@
-def build_gold_marts():
-    """
-    1. Aggregate hazard events.
-    2. Join FEMA, NRI, Census.
-    3. Build risk_feature_mart.
-    4. Write Gold tables to S3.
-    """
-    pass
